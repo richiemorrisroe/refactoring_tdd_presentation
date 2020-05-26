54b9a3db135c0cf5d1e4d249d84fbbee98917c7d
richie <richie.morrisroe@gmail.com>
2020-05-26 21:00:19 +0100

    add package and scripts along with org file

---
+++ scripts/feature_engineering.R
@@ -0,0 +1,147 @@
+with(ppr_train, BoxCoxTrans(price))
+
+ppr_train3 <- select(ppr_train2, -postal_code, 
+                     -address, 
+                     -price, -date_of_sale)
+ppr_train_s <- sample_frac(ppr_train3, size=0.1)
+ppr_test_samp <- sample_frac(ppr_train3, 
+                             size=0.05)
+train_ctrl <- trainControl(method="repeatedcv", 
+                           repeats=5)
+ppr_glmnet <- train(log_price~.,
+                    data=ppr_train_s,
+                    method="glmnet",
+                    trControl=train_ctrl)
+preds <- predict(ppr_glmnet, newdata=ppr_test_samp)
+ppr_test_samp$glmnet_preds <- preds
+
+ggplot(ppr_test_samp, aes(x=log_price^10, y=glmnet_preds^10))+geom_point()
+
+ppr_train_s2 <- filter(ppr_train_s, 
+                       log_price<=log(1e6, base=10)) %>% 
+    select(-is_big)
+ppr_glmnet2 <- train(log_price~.,
+                     data=ppr_train_s2,
+                     method="glmnet",
+                     trControl=train_ctrl)
+preds2 <- predict(ppr_glmnet2,
+                  newdata=ppr_test_samp)
+ppr_test_samp$glmnet_preds2 <- preds2
+
+ppr_gc1 <- filter(ppr_gc, year<=2016)
+ppr_gc2 <- filter(ppr_gc, year>2016)
+#github doesn't like large files
+write_csv(ppr_gc1, path="ppr_gecoded1.csv")
+write_csv(ppr_gc2, path="ppr_gecoded2.csv")
+ppr_gc <- read_csv(
+  "~/Dropbox/PPR/ppr_geocoded_till_oct2018.csv")
+names(ppr_gc)[12:24]
+
+library(sp)
+library(rgdal)
+shp <- readOGR("~/Dropbox/PPR/electoral_divisions_gps.shp")
+#df stored in this slot
+names(shp@data[,13:length(shp@data)])
+
+pobal <- read_csv("~/Dropbox/PPR/pobal_deprivation_index_ed.csv")
+names(pobal)[25:45]
+
+library(sf) #spatial simple features library
+library(sp)
+ppr_pobal <- readRDS("~/Dropbox/Code/DDS/ppr_sf_pobal2.rds")
+
+ppr_gc2 <- filter(ppr_gc, !is.na(latitude), !is.na(electoral_district))
+locs <- select(ppr_gc2, longitude, latitude)
+sp_ppr <- SpatialPointsDataFrame(locs, data=ppr_gc2, proj4string = CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+ppr_gc_sf <- st_as_sf(sp_ppr)
+
+sapply(ppr_pobal, n_distinct) %>%
+    as.data.frame() %>%
+    rownames_to_column() %>%
+    arrange(desc(`.`))
+
+select_if(as.data.frame(ppr_pobal), is.numeric) %>%
+    sapply(., function (x) 
+        data.frame(min=min(x, na.rm=TRUE), 
+                   max=max(x, na.rm=TRUE))) %>%
+    as.data.frame()
+
+sapply(ppr_pobal, function(x) 
+  sum(is.na(x))/length(x)) %>%
+    as.data.frame() %>%
+    rownames_to_column() %>%
+    dplyr::arrange(desc(`.`)) %>%
+  mutate_if(is.numeric, round, 4) %>%
+  head(n=5)
+
+num_vars <-  as.data.frame(ppr_pobal) %>%
+    na.omit() %>%
+select_if(is.numeric)
+
+bc2 <- lapply(num_vars, 
+              BoxCoxTrans, 
+              na.rm=TRUE)
+lambdas <- sapply(bc2, function (x) x[["lambda"]])
+head(lambdas)
+
+pobal_vars <- dplyr::select(num_vars, 26:64) %>% 
+    lapply(function (x) scale(x, 
+                              center=TRUE, 
+                              scale=TRUE))
+pca <- princomp(~., data=pobal_vars)
+
+screeplot(pca, type="lines")
+
+loadings <- pca$loadings
+loadings[,1:6] %>%
+  round(3) %>%
+  head()
+
+ed_count  <- ppr_pobal %>% 
+  as.data.frame() %>% 
+  group_by(electoral_district_id) %>% 
+    summarise(count=n(), median=median(price))
+ggplot(ed_count, aes(x=count))+geom_density()
+
+#otherwise the below errors out
+ppr_pobal_s <- sample_frac(ppr_pobal, size=0.1) 
+##takes a loooooonnnnng time
+lm_ed1 <- lm(log(price)~electoral_district_id, 
+             data=ppr_pobal_s)
+
+require(vtreat)
+#not a perfectly independent sample
+##don't do it like this
+ppr_train_s2 <- sample_frac(ppr_pobal, 
+                            size=0.05) 
+treat <- designTreatmentsN(ppr_train_s2, 
+                           varlist=c("electoral_district_id"), 
+                           outcomename = "price")
+train_prep <- prepare(treat, 
+                      dframe=ppr_pobal_s)
+
+ppr_pobal_test <- sample_frac(ppr_pobal, size=0.1)
+test_vtreat <- prepare(treat, ppr_pobal_test)
+glmnet_preds <- predict(glmnet_ed1, newx=as.matrix(test_vtreat), 
+                        type="response")
+predsvobs <- data.frame(preds=exp(glmnet_preds), 
+                        obs=ppr_pobal_test$price)
+ggplot(predsvobs, aes(x=obs, y=preds.s0))+geom_point()
+ggplot(predsvobs, aes(x=obs, y=preds.s60))+geom_point()
+predsvobs <- data.frame(preds=exp(glmnet_preds), obs=ppr_pobal_test$price)
+with(predsvobs, cor(preds.s60, obs))
+
+tf <- designTreatmentsN(ppr_train_s2, varlist=
+                                        setdiff(
+                                          names(ppr_train_s2), 
+                                          "price"),
+                                outcomename = "price")
+ppr_p_t <- prepare(tf,
+                   dframe=ppr_pobal_s)
+ppr_glmnet_full <- cv.glmnet(x=as.matrix(ppr_pobal_treat),
+                          y=log(ppr_pobal_s$price))
+test_vtreat_full <- prepare(treat_full, ppr_pobal_test)
+
+ggplot(predsvobs, aes(x=obs, y=X1))+geom_point()+geom_smooth()
+
+ggplot(predsvobs, aes(x=log(abs(obs-X1), base=10)))+geom_density()
---
+++ scripts/models.R
@@ -0,0 +1,49 @@
+require(lme4)
+prop_size_num <- with(train_sample, gsub("[A-zA-z]+", "", x=property_size_description))
+prop2 <- gsub("\\s+", " ", x=prop_size_num)
+train_sample$property_size_description <- fct_explicit_na(prop2)
+pprlm0 <- lm(price~property_size_description+county, data=train_sample)
+pprlm1 <- lm(price~property_size_description+county+year, data=train_sample)
+pprlm2 <- lm(price~property_size_description+county+(year)^2, data=train_sample)
+pprlm3_log <- lm(log(price)~property_size_description+county+poly(year, 2), data=train_sample)
+ppr_lmer <- lmer(price~property_size_description+county+(1|year), data=train_sample)
+ppr_lmer2 <- lmer(price~property_size_description+(1|county)+(1|year), data=train_sample)
+ppr_lmer3 <- lmer(price~property_size_description+(county|year), data=train_sample)
+
+save_model <- function(x) saveRDS(x, paste(substitute(x), ".rds", sep=""))
+
+smaller_sample <- sample_frac(prop_sample, size=.1)
+
+require(rstanarm)
+stanlm <- stan_lmer(log(price, base=10)~year+(1|county), data=smaller_sample)
+stanlm2 <- stan_lmer(log(price)~(month|year)+(1|county),
+                     data=smaller_sample)
+stanlm3 <- stan_lmer(log(price)~+postal_code+
+                         (month|year)+(1|county),
+                     data=smaller_sample)
+stanlm4 <- stan_lmer(log(price)~+vat_exclusive+not_full_market_price+
+                         (postal_code|year)+(1|county), data=smaller_sample)
+dub_sample <- filter(prop_df2, county=="Dublin") %>% sample_frac(size=0.1)
+
+stanlm_dub <- stan_lmer(log(price)~+vat_exclusive+not_full_market_price+(postal_code|year), data=dub_sample)
+
+require(rstanarm)
+stanlm <- readRDS("stanlm.rds")
+stanlm2 <- readRDS("stanlm2.rds")
+stanlm3 <- readRDS("stanlm3.rds")
+stanlm_dub <- readRDS("stanlm_dub.rds")
+
+stanlm <- stan_lmer(log(price)~year+(1|county), data=smaller_sample)
+
+dubonly <- filter(ppr_gc2, ppr_county=="Dublin")
+dubonly2 <- mutate(dubonly, ed_id=as.numeric(electoral_district_id))
+pobal_plus_ppr_dublin <- inner_join(pobal2, dubonly2, by=c("ID06"="ed_id"))
+dublin_model_sample <- sample_frac(pobal_plus_ppr_dublin, 0.1)
+require(caret)
+dub_trainind <- with(dublin_model_sample, createDataPartition(price, p=0.7, times=1, list=FALSE))
+dub_train <- dublin_model_sample[trainind,]
+dub_test <- dublin_model_sample[-trainind,]
+pobal_locs <- select(pobal_plus_ppr, latitude, longitude)
+ sp_ppr_pobal <- SpatialPointsDataFrame(pobal_locs, data=ppr_plus_pobal, proj4string = CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+sf_ppr_pobal <- st_as_sf(sp_ppr_pobal)
+ppr_plus_shapefile <- merge(sp_ppr_pobal, dubspatial, by.x="ed_id", by.y="CSOED")
