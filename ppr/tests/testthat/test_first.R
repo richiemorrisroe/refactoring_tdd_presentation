e9661c6a1e135bee6ea4e39d80c1fa857a90b01d
richie <richie.morrisroe@gmail.com>
2020-06-04 12:43:38 +0100

    add log price test and some points for presentation

--- ppr/tests/testthat/test_first.R
+++ ppr/tests/testthat/test_first.R
@@ -5,9 +5,10 @@ test_that('load_data returns a tibble',
   {expect_equal(
             class(dat)[1], "tbl_df")})
 
-finished_names <- as.character(c("date_of_sale_dd_mm_yyyy",   "address", "postal_code", "county",                   
-  "price", "not_full_market_price","vat_exclusive",
-  "description_of_property",  "property_size_description"))
+finished_names <- as.character(c("date_of_sale_dd_mm_yyyy",
+                                 "address", "postal_code", "county",                   
+                                 "price", "not_full_market_price","vat_exclusive",
+                                 "description_of_property",  "property_size_description"))
 
 test_that('normalise_names works',
           {expect_equal(finished_names, names(normalise_names(dat)))})
@@ -18,9 +19,15 @@ test_that('fix price returns the correct price',
 test_that('fix price returns the correct price',
           expect_equal(3000000.01, fix_price("3,000,000.01")))
 
-test_df_output  <- readr::read_csv("../mark_values_as_large_test_data_done.csv")
-test_df_input  <- readr::read_csv("../mark_values_as_large_test_data.csv")
+test_df_output  <- readr::read_csv("~/Dropbox/Code/Rlang/refactoring_and_tdd/ppr/mark_values_as_large_test_data_done.csv")
+test_df_input  <- readr::read_csv("~/Dropbox/Code/Rlang/refactoring_and_tdd/ppr/mark_values_as_large_test_data.csv")
 test_that('mark values as large works', {
   expect_equal(mark_values_as_large(test_df_input, large = 1e6),
                test_df_output)
 })
+
+test_that('log(price)< price',{
+          new_df <-  log_column(df, column)
+          expect_lt(new_df$log_price,
+                    df$price)}
+          )
--- r-build.R
+++ r-build.R
@@ -1,4 +1,9 @@
 require(devtools)
 if (!dir.exists("ppr")) {
-  devtools::create("ppr")
+  usethis::create_package("ppr")
 }
+
+usethis::use_package("dplyr")
+usethis::use_package("rlang")
+usethis::use_package("readxl")
+usethis::use_data_raw()
