430d6ecfac75a18d877c6a84c9a45abd7124a85b
richie <richie.morrisroe@gmail.com>
2020-06-05 19:17:55 +0100

    fix indentation, add output dataframes for comparison

--- scripts/prep_modelling.R
+++ scripts/prep_modelling.R
@@ -2,7 +2,52 @@ library(tidyverse)
 library(readxl)
 ppr <- read_excel("~/Dropbox/PPR/PPR-ALL.xlsx", sheet = "PPR-ALL")
 names(ppr)
+normalise_names <- function(df) {
+        nms <- names(df)
+        normed <- iconv( # covert from one encoding to another
+                tolower(
+                        gsub("([[:space:]]|[[:punct:]])+", "_",
+                                x = nms
+                        )
+                ),
+                "latin1", # from encoding
+                "ASCII", # to encoding
+                sub = ""
+        )
+        drop_usc <- gsub("([a-z_])_*$",
+                         "\\1", x = normed) # drop extra underscores
+        names(df) <- drop_usc # update names
+        df # return dataframe
+}
+fix_price <- function(x) {
+        nopunct <- gsub(",", "", x = x)
+        nums <- as.numeric(
+                iconv(nopunct, "latin1",
+                        "ASCII",
+                        sub = ""
+                )
+        )
+}
 
+generate_bootstrap_results <- function(df, ind) {
+        # results list (always generate something to hold your results first)
+        trainresults <- vector(mode = "list", length = 10)
+        for (i in 1:length(ind)) {
+                nm <- names(ind[i])
+                train <- df[ind[[i]], ]
+                test <- df[-ind[[i]], ]
+                model <- lm(log_price ~ property_size_description + year,
+                        data = train
+                )
+                preds <- predict(model,
+                        newdata = test, type = "response",
+                        na.action = na.exclude
+                )
+                trainresults[[i]] <- preds
+        }
+        names(trainresults) <- names(ind)
+        trainresults
+}
 
 ppr2 <- normalise_names(ppr)
 
@@ -218,3 +263,4 @@ generate_bootstrap_results <- function(df, ind) {
 g <- generate_bootstrap_results(ppr_train2, ppr_bootstrap_indices)
 hist(sapply(g, mean)^10)
 
+readr::write_csv(ppr_train2, file="prep_modelling_output_old.csv")
