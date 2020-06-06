066fe3efd602051985b36467d3b7b9d7133d84b6
richie <richie.morrisroe@gmail.com>
2020-06-06 14:52:19 +0100

    change output name of file for integration testing

--- ppr/inst/prep_modelling.R
+++ ppr/inst/prep_modelling.R
@@ -265,5 +265,5 @@ generate_bootstrap_results <- function(df, ind) {
 g <- generate_bootstrap_results(ppr_train2, ppr_bootstrap_indices)
 hist(sapply(g, mean)^10)
 
-readr::write_csv(ppr_train2, path = "prep_modelling_output_old.csv")
-## readr::write_csv(ppr_train2, path = "prep_modelling_output_refactor.csv")
+## readr::write_csv(ppr_train2, path = "prep_modelling_output_old.csv")
+readr::write_csv(ppr_train2, path = "prep_modelling_output_refactor.csv")
